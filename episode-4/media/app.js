be9b0bd62fa815fb74e0082216686ddddd261458
Derick Bailey <derickbailey@gmail.com>
2015-12-07 13:00:20 -0600

    added start of episode 4

---
+++ episode-4/media/app.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var raygun = require('raygun');
+var epa = require("epa").getEnvironment();
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.status(404);
+  res.send("");
+});
+
+// error handlers
+// --------------
+
+var raygunClient = new raygun.Client().init({ 
+  apiKey: epa.get("raygun")
+});
+
+app.use(raygunClient.expressHandler);
+
+process.on('uncaughtException', function(err) {
+  raygunClient.send(err);
+  console.log(err.stack);
+  process.exit(-1);
+});
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
